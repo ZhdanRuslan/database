06f9c9786090fabe00f83f7b91e653a1286e5a84
Zhdan <zhdanruslan@i.ua>
2018-03-21 09:53:13 +0200

    simply connection to MySQL

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Go MySQL")
+
+	db, err := sql.Open("mysql", "root:password@/mydb")
+
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO persons VALUES ( 2, 'Team', 'Member' )")
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer insert.Close()
+}
